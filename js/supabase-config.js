375f5235f8fbc5072f4113f81ba69bc8ea3516f3
incogni100x <duche20001@gmail.com>
2026-08-14 19:20:58 +0100

    Update values

--- js/supabase-config.js
+++ js/supabase-config.js
@@ -1,8 +1,8 @@
 window.APP_CONFIG = {
-  supabaseUrl: "https://tylafuyzhotmqkyxwvrp.supabase.co",
-  supabaseRestUrl: "https://tylafuyzhotmqkyxwvrp.supabase.co/rest/v1/",
+  supabaseUrl: "https://jttemxvybpcubmnyqvdr.supabase.co",
+  supabaseRestUrl: "https://jttemxvybpcubmnyqvdr.supabase.co/rest/v1/",
   supabaseAnonKey:
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InR5bGFmdXl6aG90bXFreXh3dnJwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzcyMDcyMDcsImV4cCI6MjA5Mjc4MzIwN30.dw56GArQRDHHCjCzFxuGsGLMeT-YEgv2IWg9VJWAfOk",
-  siteUrl: "https://pythoncyberhacks.com",
-  adminEmail: "support@pythoncyberhacks.com",
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imp0dGVteHZ5YnBjdWJtbnlxdmRyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODY3MjM2NTksImV4cCI6MjEwMjI5OTY1OX0.9My81uZQZ26dueaE1hx_BRmGGIQe3NUDVybP_wOcMCw",
+  siteUrl: "https://veritracegroup.com",
+  adminEmail: "support@veritracegroup.com.com",
 };
